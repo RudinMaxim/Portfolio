5cce2a8cabf88cfd3a8fbbcb103907a23a2d470b
Rudin Maxim <rudin.maksimka@bk.ru>
2022-03-13 01:14:52 +0500

    1

--- main.js
+++ main.js
@@ -2,40 +2,39 @@ $(document).ready(function() {
     $(window).scroll(function() {
         // sticky navbar on scroll script
         if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
+            $(".navbar").addClass("sticky");
         } else {
-            $('.navbar').removeClass("sticky");
+            $(".navbar").removeClass("sticky");
         }
 
         // scroll-up button show/hide script
         if (this.scrollY > 500) {
-            $('.scroll-up-btn').addClass("show");
+            $(".scroll-up-btn").addClass("show");
         } else {
-            $('.scroll-up-btn').removeClass("show");
+            $(".scroll-up-btn").removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function() {
-        $('html').animate({ scrollTop: 0 });
+    $(".scroll-up-btn").click(function() {
+        $("html").animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
+        $("html").css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function() {
+    $(".navbar .menu li a").click(function() {
         // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
+        $("html").css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function() {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
+    $(".menu-btn").click(function() {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
     });
 
-
     // owl carousel script
-    $('.carousel').owlCarousel({
+    $(".carousel").owlCarousel({
         margin: 20,
         loop: true,
         autoplay: true,
@@ -44,34 +43,60 @@ $(document).ready(function() {
         responsive: {
             0: {
                 items: 1,
-                nav: false
+                nav: false,
             },
             600: {
                 items: 2,
-                nav: false
+                nav: false,
             },
             1000: {
                 items: 3,
-                nav: false
-            }
-        }
+                nav: false,
+            },
+        },
+    });
+});
+
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        center: true,
+        navText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>",
+        ],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1,
+            },
+            1000: {
+                items: 3,
+            },
+        },
     });
 });
 
 const skillsContener = document.getElementsByClassName('skills__continer'),
-    skillsHeader = document.getElementsByClassName('skills__header')
+    skillsHeader = document.getSelection('skills__header');
 
 function toggleSkills() {
-    let itemCLass = this.parentNode.className
+    let itemCLass = this.parentNode.ClassName;
 
     for (i = 0; i < skillsContener.length; i++) {
-        skills__continer[i].className = 'skills__continer skills__close'
+        skills__continer[i].className = 'skills__continer skills__close';
     }
-    if (itemCLass === 'skills__continer skills__open') {
-        this.parentNode.className = 'skills__continer skills__open'
+    if (itemCLass == 'skills__continer skills__close') {
+        this.parentNode.className = 'skills__continer skills__open';
     }
 }
-
 skillsHeader.forEach((el) => {
-    el.addEventListener('click', toggleSkills)
-})
+    el.addEventListener('click', toggleSkills);
+});
