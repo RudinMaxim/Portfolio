5ea771affe115b2452b672f21c552cd7903041c3
Maxim Rudin <>
2024-03-27 21:58:51 +0500

    add ui

--- src/app/[locale]/project/module/project/ProjectViwe.tsx
+++ src/app/[locale]/project/module/project/ProjectViwe.tsx
@@ -1,15 +1,15 @@
-'use client';
-import { useProject } from './Project.hook';
-import style from './Project.module.scss';
-import { ProjectList } from './components';
+"use client";
+import { useProject } from "./Project.hook";
+import style from "./Project.module.scss";
+import { ProjectList } from "./components";
 
 export function Project() {
-	const props = useProject();
+  const props = useProject();
 
-	return (
-		<section className={style.Project}>
-			<h1 style={{ textAlign: 'center', fontSize: '3.5rem' }}>Project</h1>
-			<ProjectList projects={props.projects} />
-		</section>
-	);
+  return (
+    <section className={style.Project}>
+      <h1 style={{ textAlign: "center", fontSize: "3.5rem" }}>Project</h1>
+      <ProjectList projects={props.projects} />
+    </section>
+  );
 }
